043039e0293a55a71067a1bd680222d4266e4583
Md. Mehedi Hasan <mehedibarisal1998@gmail.com>
2022-10-13 13:02:25 +0600

    added routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const userRouter = require('./routes/userRoutes');
+const noteRouter = require('./routes/noteRoutes');
 const app = express();
 const PORT = 8000;
+
+app.use("/users", userRouter);
+app.use("/note", noteRouter);
 
 app.get('/', (req, res) => {
     res.send("Hello BS23")
