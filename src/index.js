2d3c2f5bccce112de582aff20d4575acdc7ef77d
Md. Mehedi Hasan <mehedibarisal1998@gmail.com>
2022-10-13 12:36:44 +0600

    complete local API setup & nodemon

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.get('/', (req, res) => {
+    res.send("Hello BS23")
+})
+
+app.get('/test', (req, res) => {
+    res.send("This is test page")
+})
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+})
